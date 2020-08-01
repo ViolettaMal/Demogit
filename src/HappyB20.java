f0d18bc4bc8d2453d0ad30d7e92b4bbf7caac42c
ViolettaMal <68824703+ViolettaMal@users.noreply.github.com>
2020-08-01 15:34:26 -0500

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,10 +2,7 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("Happy B20");
 
-        int i = 5;
-        if (i>0){
-            System.out.println("You guys are awesome!");
-        }
-
+        //some code here
+        
     }
 }
