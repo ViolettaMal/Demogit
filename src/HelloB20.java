f781596b81499dd042d4d5c4139c6b354dd60d86
ViolettaMal <malakhovskaya@yahoo.com>
2020-08-01 10:52:12 -0500

    update HappyB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,5 +3,10 @@ public class HelloB20 {
         System.out.println("Hello B20, it  is my pleasure to be with you!");
 
             //added a line
+
+        int i = 5;
+        if (i>0){
+            System.out.println("You guys are awesome!");
+        }
     }
 }
